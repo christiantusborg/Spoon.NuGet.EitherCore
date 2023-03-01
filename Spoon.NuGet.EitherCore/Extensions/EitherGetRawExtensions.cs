a2ee61c466845418f8bf954a4cb73da0a3dcd56d
Christian Tusborg <gitlab.com@terbium.dk>
2023-03-01 01:29:04 +0100

    Added TryGetResponseRaw and ToResult fixed

---
+++ Spoon.NuGet.EitherCore/Extensions/EitherGetRawExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Spoon.NuGet.EitherCore.Extensions;
+
+using Enums;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+
+/// <summary>
+/// </summary>
+public static class EitherGetRawExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="either"></param>
+    /// <param name="responseRaw"></param>
+    /// <param name="iResult"></param>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static bool TryGetResponseRaw<TResponse>(this Either<TResponse> either, out TResponse? responseRaw, out IResult? iResult )
+    {
+        if (either.EitherEnum != EitherEnum.Success)
+        {
+            iResult = either.GetFaulted().ToResult();
+            responseRaw = default;
+            return false;
+        }
+
+        if (either.success is null)
+            throw new ArgumentNullException("Getinge.NuGet.EitherCore.Exceptions.ExceptionBase" + nameof(either.success));
+
+        responseRaw = either.success;
+        iResult = null;
+        return true;
+
+    }    
+}
--- Spoon.NuGet.EitherCore/Extensions/EitherResultExtensions.cs
+++ Spoon.NuGet.EitherCore/Extensions/EitherResultExtensions.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 /// </summary>
 public static class EitherResultExtensions
 {
+/*
     /// <summary>
     ///     Converts to actionresult.
     /// </summary>
@@ -37,6 +38,40 @@ public static class EitherResultExtensions
         }
 
         var responseObject = Activator.CreateInstance(typeof(TResponse));
+
+        return Results.Ok(responseObject);
+    }
+*/
+    /// <summary>
+    /// </summary>
+    /// <param name="either"></param>
+    /// <param name="type"></param>
+    /// <param name="noContent"></param>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IResult ToResult<TResponse>(this Either<TResponse> either, Type type, bool noContent = false)
+    {
+        if (either.EitherEnum != EitherEnum.Success)
+            return either.GetFaulted().ToResult();
+
+        if (noContent)
+            return Results.NoContent();
+
+        if (either.success is null)
+            throw new ArgumentNullException("Getinge.NuGet.EitherCore.Exceptions.ExceptionBase" + nameof(either.success));
+
+        var properties = typeof(TResponse).GetProperties();
+
+        if (properties.Length > 0)
+        {
+            var responseTyped = Activator.CreateInstance(type);
+            var response = either.success.Adapt(responseTyped);
+
+            return Results.Ok(response);
+        }
+
+        var responseObject = Activator.CreateInstance(type);
 
         return Results.Ok(responseObject);
     }
